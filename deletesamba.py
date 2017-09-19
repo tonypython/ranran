cbf1834e180c36453dfc0d54690fc6737546da24
tonypython <32100668+tonypython@users.noreply.github.com>
2017-09-19 23:37:59 +0800

    Update deletesamba.py

--- deletesamba.py
+++ deletesamba.py
@@ -1,29 +1,4 @@
 import paramiko
-'''
-port=22
-username=["root"]
-password=["!2ntsj","123456"]
-a="!2ntsj"
-ip=["172.23.13.34"]
-test=[]
-try:
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(ip[0],port,username[0], password[0])
-	stdin, stdout, stderr = ssh.exec_command('echo %s | passwd --stdin root'%(a))
-	if stderr.read()=="":
-		print ("%s 密码修改成功." %(ip[0]))
-except TimeoutError:
-	print ("连接超时")
-except paramiko.ssh_exception.AuthenticationException:
-	print ("原始密码错误")
-except ConnectionRefusedError:
-        print ("非linux主机")
-finally:
-        ssh.close()
-        print ("xixihaha")
-'''
-
 d=[]
 f=[]
 file = open(r"C:\Users\Administrator\Desktop\pvg.txt")
@@ -32,7 +7,6 @@ for line in file:
         line=line.strip()
         d.append(line)
 file.close()
-
 
 def ceshi(ip,port,username,password):
 	try:
@@ -44,8 +18,6 @@ def ceshi(ip,port,username,password):
 			return ("%s \t samba删除成功." %(ip))
 		else:
 						return ("%s \t 不包含samba"%(ip))
-	except TimeoutError:
-	       return ("%s \t 连接超时"%(ip))
 	except paramiko.ssh_exception.AuthenticationException:
 	       return ("%s \t 原始密码错误"%(ip))
 	except ConnectionRefusedError:
@@ -54,15 +26,13 @@ def ceshi(ip,port,username,password):
 		ssh.close()
 port=22
 username=["root"]
-password=["!2ntsj"]
-
+password=["xxxx"]
 
 for i in d:
         v=ceshi(i,port,username[0],password[0])
         print (v)
         f.append(v)
-
-
+	
 output = open(r"C:\Users\Administrator\Desktop\result.txt", 'wt')
 for i in f:
         output.write(i+'\n')
